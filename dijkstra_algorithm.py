aa4104d353a406375a230be57ed3bc2b6a88831b
BohdanZasymovych <bohdan.zasymovych@gmail.com>
2024-11-22 22:35:33 +0200

    queue implementation through list replased with implementation with PriorityQueue

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -1,4 +1,6 @@
 '''dijkstra algorithm'''
+from queue import PriorityQueue
+
 maze_matrix = [
     [2, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -52,34 +54,55 @@ def matrix_to_adj_dict(maze: list[list[int]]):
     return adjacent_dict, start, end
 
 
-def compare_dist(point: tuple[tuple, int], graph: dict) -> bool:
-    return point[1] <= graph[point[0]][0]
+# def compare_dist(point: tuple[tuple, int], graph: dict) -> bool:
+#     return point[1] <= graph[point[0]][0]
 
 
-def get_min_dist_node(graph: dict, queue: list) -> tuple:
-    return min(queue, key=lambda el: el[1] if compare_dist(el, graph) else float('inf'))
+# def get_min_dist_node(graph: dict, queue: list) -> tuple:
+#     return min(queue, key=lambda el: el[1] if compare_dist(el, graph) else float('inf'))
+
+
+# def dijkstra(maze: dict[tuple: tuple], start: tuple, end: tuple) -> dict:
+#     graph = {node: [float('inf'), False, None] for node in maze}
+#     graph[start] = [0, True, start]
+#     queue = [(start, 0)]
+#     while True:
+#         cur_node = get_min_dist_node(graph, queue)
+#         if cur_node[0] == end:
+#             return graph
+#         graph[cur_node[0]][1] = True
+#         queue.remove(cur_node)
+#         for node in maze[cur_node[0]]:
+#             if graph[node][1]:
+#                 continue
+#             if graph[node][0] > cur_node[1] + 1:
+#                 graph[node][0] = cur_node[1] + 1
+#                 graph[node][2] = cur_node[0]
+#                 queue.append((node, graph[node][0]))
+#         if not queue:
+#             return -1
 
 
 def dijkstra(maze: dict[tuple: tuple], start: tuple, end: tuple) -> dict:
     graph = {node: [float('inf'), False, None] for node in maze}
     graph[start] = [0, True, start]
-    queue = [(start, 0)]
+    nodes_queue = PriorityQueue()
+    nodes_queue.put((0, start))
     while True:
-        cur_node = get_min_dist_node(graph, queue)
-        if cur_node[0] == end:
+        dist_to_cur_node, cur_node = nodes_queue.get(timeout=5)
+        if cur_node == end:
             return graph
-        graph[cur_node[0]][1] = True
-        queue.remove(cur_node)
-        for node in maze[cur_node[0]]:
+        graph[cur_node][1] = True
+        for node in maze[cur_node]:
             if graph[node][1]:
                 continue
-            if graph[node][0] > cur_node[1] + 1:
-                graph[node][0] = cur_node[1] + 1
-                graph[node][2] = cur_node[0]
-                queue.append((node, graph[node][0]))
-        if not queue:
+            if graph[node][0] > dist_to_cur_node + 1:
+                dist_to_node = dist_to_cur_node + 1
+                graph[node][0] = dist_to_node
+                graph[node][2] = cur_node
+                nodes_queue.put((dist_to_node, node))
+        if nodes_queue.empty():
             return -1
-    return -1
 
 
 def reconstruct_path(graph: dict, start: tuple, end: tuple) -> list[tuple]:
